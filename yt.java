34c166e005c6b1e5a61c431e06dc32198a8495c6
yathin <yathinchelluri45@gmail.com>
2025-06-13 19:14:53 +0530

    Add files via upload

---
+++ yt.java
@@ -0,0 +1,5 @@
+function getMessage() {
+    const message = document.getElementById("userMessage").value;
+    console.log("User entered:", message);
+    // You can now do anything with the message, like display or send to server
+}
