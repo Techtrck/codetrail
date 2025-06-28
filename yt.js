78aa55f2bde615637344f0c8b84a51c2387aad45
yathin <yathinchelluri45@gmail.com>
2025-06-28 16:11:54 +0530

    Delete yt.js

--- yt.js
+++
@@ -1,5 +0,0 @@
-function getMessage() {
-    const message = document.getElementById("userMessage").value;
-    console.log("User entered:", message);
-    // You can now do anything with the message, like display or send to server
-}
